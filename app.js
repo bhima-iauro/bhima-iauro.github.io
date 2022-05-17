10112c999d19e0620c1bd728ddef536fc410907e
Bhimrao <bhimrao.patange@iauro.com>
2022-05-17 15:35:03 +0530

    sdf

---
+++ app.js
@@ -0,0 +1,8 @@
+function show1(){
+      if(document.getElementById('option-1').checked===true){
+          document.getElementById('right-container').style.display='none';
+      }
+      if(document.getElementById('option-2').checked===true){
+          document.getElementById('right-container').style.display='block';
+      }
+  }
